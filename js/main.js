2147fbcb71e1ff0697a6b0585ece3bcbc237800c
mrhrifat <mrhrifat383759@gmail.com>
2021-03-28 16:23:50 +0600

    Fixing nav

--- js/main.js
+++ js/main.js
@@ -1,60 +1,17 @@
-$(document).ready(function(){
-
-	// Photo Filter
-	let $btns = $('#portfolio .button-group button');
-
-	$btns.click(function(e){
-
-		$('#portfolio .button-group button').removeClass('active');
-
-		e.target.classList.add('active');
-
-
-		let selector = $(e.target).attr('data-filter');
-
-		$('#portfolio .grid').isotope({
-			filter:selector
-		});
-
-		return false;
-	});
-
-	// Photo Popup
-	$('#portfolio .button-group #btn1').trigger('click');
-
-	$('#portfolio .grid .test-popup-link').magnificPopup({
-		type: 'image',
-		gallery:{enabled:true}
-	});
-
-
-	//Owl Carousel
-	$('#about-me .carousel-items .owl-carousel').owlCarousel({
-		loop:true,
-		dots:true,
-		autoplay:true,
-		responsive:{
-			0:{
-				items:1
-			},
-			544:{
-				items:2
-			}
-		}
-	});
+$(document).ready(function () {
 
 
 	//Navbar
-	let nav_offset_top=$('#header').height()+50;
+	let nav_offset_top = $('#header').height() + 50;
 
-	function navbarFixed(){
-		if($('#header').length){
-			$(window).scroll(function(){
+	function navbarFixed() {
+		if ($('#header').length) {
+			$(window).scroll(function () {
 				let scroll = $(window).scrollTop();
 
-				if(scroll >= nav_offset_top){
+				if (scroll >= nav_offset_top) {
 					$('#header .main').addClass('nav_fixed');
-				}else{
+				} else {
 					$('#header .main').removeClass('nav_fixed');
 				}
 			});
